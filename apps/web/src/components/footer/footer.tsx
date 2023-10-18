f7aaedb83834bf42ccdf42292f54582311bf15ec
Manraj <manraj2712@gmail.com>
2023-10-19 00:15:34 +0530

    added .env.example

--- apps/web/src/components/footer/footer.tsx
+++ apps/web/src/components/footer/footer.tsx
@@ -35,8 +35,11 @@ export function Footer() {
       </div>
       <div className="flex flex-row mx-4 flex-wrap gap-x-2 justify-center items-center">
         {tabarr.map((item, index) => (
-          <div className="flex flex-row flex-wrap gap-x-2 text-neutral-300">
-            <Link href={item.href} key={index}>
+          <div
+            key={index}
+            className="flex flex-row flex-wrap gap-x-2 text-neutral-300"
+          >
+            <Link href={item.href}>
               {item.name}
             </Link>
             <p>{index !== tabarr.length - 1 ? "|" : ""}</p>
