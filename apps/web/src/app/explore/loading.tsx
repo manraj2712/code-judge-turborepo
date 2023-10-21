f9cf144dfa2bff9a1be7d6efd9ab255ce2b1985f
Aarjav Jain <anshjain638@gmail.com>
2023-10-21 21:37:54 +0530

    styled shimmer

--- apps/web/src/app/explore/loading.tsx
+++ apps/web/src/app/explore/loading.tsx
@@ -1,94 +1,95 @@
 import React from 'react'
 const columns = [
-    { id: "status", label: "Status" },
-    { id: "acceptance", label: "Acceptance" },
-    { id: "title", label: "Title" },
-    { id: "difficulty", label: "Difficulty" },
-  ];
+  { id: "status", label: "Status" },
+  { id: "acceptance", label: "Acceptance" },
+  { id: "title", label: "Title" },
+  { id: "difficulty", label: "Difficulty" },
+];
 const fakeData = Array.from({ length: 8 }).map((_, index) => ({
-    id: index + 1,
-    title: "Loading...",
-    status: "Loading...",
-    difficulty: "Loading...",
-  }));
-const loading = () => {
+  id: index + 1,
+  title: "Loading...",
+  status: "Loading...",
+  difficulty: "Loading...",
+}));
+const Loading = () => {
   return (
     <div className="flex flex-row w-full h-full">
-        <div className="flex flex-col sm:block w-80 xl:w-56 p-3 bg-neutral-900 shadow duration-300 overflow-y-auto">
+      <div className="flex flex-col sm:block w-80 xl:w-56 p-3 animate-pulse bg-neutral-900 shadow duration-300 overflow-y-auto">
         <table>
-        <tbody>
-                {fakeData.map((i) => (
-                  <tr
-                    key={i.id}
-                    className="odd:bg-neutral-900 text-gray-200"
-                  >
-                    <td className="hidden lg:table-cell px-6 py-4 text-base">
-                      {i.status} 
-                    </td>
-                    </tr>
-                ))}
-                </tbody>
-                </table>
-            </div>
-        <div className="flex flex-col w-full px-3">
-    <div className="">
-      <div className="p-1.5 min-w-full inline-block align-middle">
-        <div className="overflow-x-hidden">
-      
-      <table className="table-fixed overflow-y-scroll lg:table-auto min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-              <thead>
-                <tr>
-                  {columns.map((column, index) => (
-                    <th
-                      key={column.id}
-                      className={`lg:px-6 py-3 ${
-                        index === columns.length - 1
-                          ? "text-center"
-                          : "text-left"
-                      } ${
-                        ["acceptance", "status"].includes(column.id)
-                          ? "hidden lg:table-cell"
-                          : ""
-                      } ${
-                        column.id == "title" ? "w-[70%]" : "w-auto"
-                      } text-base font-medium text-gray-500 dark:text-gray-200`}
-                    >
-                      {column.label}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {fakeData.map((i) => (
-                  <tr
-                    key={i.id}
-                    className="odd:bg-neutral-900 text-gray-200"
-                  >
-                    <td className="hidden lg:table-cell px-6 py-4 text-base">
-                      {i.status} 
-                    </td>
-                    <td className="hidden lg:table-cell px-6 py-4 text-left text-base">
-                      {i.difficulty}
-                    </td>
-
-                    <td className="w-[70%] lg:w-auto hide-multi-line px-2 lg:px-6 py-4 hover:cursor-pointer hover:text-blue-700 text-base">
-                        {i.title}
-                    </td>
-                    <td
-                      className='px-6 py-4 text-center text-base '>
-                      {i.difficulty}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-      </table>
+          <tbody>
+            {fakeData.map((i) => (
+              <tr
+                key={i.id}
+                className="odd:bg-neutral-900 text-gray-200 "
+              >
+                <td>
+                 <div className='hidden lg:flex mb-4 bg-slate-500 px-20 py-1 text-base'></div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
+      <div className="flex flex-col w-full px-3">
+        <div className="animate-pulse">
+          <div className="p-1.5 min-w-full inline-block align-middle">
+            <div className="overflow-x-hidden">
+
+              <table className="table-fixed overflow-y-scroll lg:table-auto min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                <thead>
+                  <tr>
+                    {columns.map((column, index) => (
+                      <th
+                        key={column.id}
+                        className={`lg:px-6 py-3 ${index === columns.length - 1
+                            ? "text-center"
+                            : "text-left"
+                          } ${["acceptance", "status"].includes(column.id)
+                            ? "hidden lg:table-cell"
+                            : ""
+                          } ${column.id == "title" ? "w-[70%]" : "w-auto"
+                          } text-base font-medium text-gray-500 dark:text-gray-200`}
+                      >
+                        {column.label}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {fakeData.map((i) => (
+                    <tr
+                      key={i.id}
+                      className="odd:bg-neutral-900 text-gray-200"
+                    >
+                      <td className="hidden lg:table-cell px-6 py-4 text-base">
+                        <div className='hidden lg:table-cell h-1 w-[30%] bg-slate-500 px-10 py-1 text-base'></div>
+                        {/* {i.status} */}
+                      </td>
+                      <td className="hidden lg:table-cell px-6 py-4 text-left text-base">
+                        {/* {i.difficulty} */}
+                        <div className='hidden lg:table-cell h-1 w-[30%] bg-slate-500 px-10 py-1 text-base'></div>
+                      </td>
+
+                      <td className="w-[70%] lg:w-auto hide-multi-line px-2 lg:px-6 py-4 hover:cursor-pointer hover:text-blue-700 text-base">
+                        {/* {i.title} */}
+                        <div className='hidden lg:table-cell h-1 w-[30%] bg-slate-500 px-10 py-1 text-base'></div>
+                      </td>
+                      <td
+                        className='px-6 py-4 text-center text-base '>
+                        {/* {i.difficulty} */}
+                        <div className='hidden lg:table-cell h-1 w-[30%] bg-slate-500 px-10 py-1 text-base'></div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
 
 
-export default loading
+export default Loading
