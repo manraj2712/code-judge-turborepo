596e54d365d808408edb814af589bbe534f020ea
Manraj <65106261+manraj2712@users.noreply.github.com>
2024-04-21 22:43:08 +0530

    Update profile section with user progress

--- apps/web/src/components/profile/stats.tsx
+++ apps/web/src/components/profile/stats.tsx
@@ -1,45 +1,147 @@
-import React from 'react'
-import ProgressBar from './progressbar'
-import DonutProgressBar from './donoughtChart'
-import logo from '../../../public/images/logo.png'
-import Image from 'next/image'
-import { getServerSession } from 'next-auth';
-import './style.css'
+import React from "react";
+import ProgressBar from "./progressbar";
+import DonutProgressBar from "./donoughtChart";
+import logo from "../../../public/images/logo.png";
+import Image from "next/image";
+import { getServerSession } from "next-auth";
+import "./style.css";
+import { Difficulty, Status, prisma } from "@manraj2712/database";
 const stats = async () => {
   const session = await getServerSession();
-  const Total_Questions = 200;
-  const questions_completed = 100
+  const res = await prisma.problem.groupBy({
+    by: ["difficulty"],
+    _count: {
+      id: true,
+    },
+  });
+
+  const totalQuestionsByDifficultyMap = res.reduce(
+    (acc, curr) => {
+      acc[curr.difficulty] = curr._count.id;
+      return acc;
+    },
+    {
+      EASY: 0,
+      MEDIUM: 0,
+      HARD: 0,
+    }
+  );
+  let userSubmissionsByDifficulty = {
+    EASY: 0,
+    MEDIUM: 0,
+    HARD: 0,
+  };
+
+  if (session && session.user && session.user.email) {
+    // fetch questions completed by user based on difficulty from submissions table use groupBy and check difficulty from problem table
+    const submissions = await prisma.submission.findMany({
+      where: {
+        user: {
+          email: session.user.email,
+        },
+        status: Status.AC,
+      },
+      include: {
+        problem: true,
+      },
+    });
+
+    userSubmissionsByDifficulty = submissions.reduce(
+      (acc, submission) => {
+        const difficulty = submission.problem.difficulty;
+        if (acc[difficulty] === undefined) {
+          acc[difficulty] = 0;
+        }
+        acc[difficulty]++;
+        return acc;
+      },
+      {
+        EASY: 0,
+        MEDIUM: 0,
+        HARD: 0,
+      }
+    );
+  }
+
   return (
-    <div className='flex flex-col w-full'>
+    <div className="flex flex-col w-full">
       <div className="flex flex-row justify-between mb-3">
+        <div className="justify-start font-semibold text-neutral-200 text-xs">
+          Solved Problems
+        </div>
 
-        <div className='justify-start font-semibold text-neutral-200 text-xs' >Solved Problems</div>
-
-        <div className='justify-end font-semibold text-neutral-200 text-xs mr-4 ' style={{ color: "grey" }}>rank</div>
+        <div
+          className="justify-end font-semibold text-neutral-200 text-xs mr-4 "
+          style={{ color: "grey" }}
+        >
+          rank
+        </div>
       </div>
-      <div className='flex flex-row gap-4 h-full w-full '>
-
-        <div className='flex justify-center items-center w-full h-fit donut'><DonutProgressBar questions_completed={questions_completed} Total_Questions={Total_Questions} /></div>
-        <div className='flex flex-col h-fit gap-2 w-full mr-3'>
-          <div className='flex flex-row justify-between mr-3'>
-            <div className='justify-start font-medium text-xs text-neutral-200'  >Easy</div>
-            <div className='justify-end font-semibold text-xs text-neutral-200'  >50/100</div>
+      <div className="flex flex-row gap-4 h-full w-full ">
+        <div className="flex justify-center items-center w-full h-fit donut">
+          <DonutProgressBar
+            questionsCompletedByUser={
+              userSubmissionsByDifficulty.EASY +
+              userSubmissionsByDifficulty.MEDIUM +
+              userSubmissionsByDifficulty.HARD
+            }
+            totalQuestions={
+              totalQuestionsByDifficultyMap.EASY +
+              totalQuestionsByDifficultyMap.MEDIUM +
+              totalQuestionsByDifficultyMap.HARD
+            }
+          />
+        </div>
+        <div className="flex flex-col h-fit gap-2 w-full mr-3">
+          <div className="flex flex-row justify-between mr-3">
+            <div className="justify-start font-medium text-xs text-neutral-200">
+              Easy
+            </div>
+            <div className="justify-end font-semibold text-xs text-neutral-200">
+              {userSubmissionsByDifficulty.EASY}/
+              {totalQuestionsByDifficultyMap.EASY}
+            </div>
           </div>
-          <ProgressBar Total_Questions={100} questions_completed={50} color={"#29A01A"} bgcolor={"#294d35"} />
-          <div className='flex flex-row justify-between mr-3'>
-            <div className='justify-start font-medium text-xs text-neutral-200' >Medium</div>
-            <div className='justify-end font-semibold text-xs text-neutral-200' >20/40</div>
+          <ProgressBar
+            Total_Questions={totalQuestionsByDifficultyMap.EASY}
+            questions_completed={userSubmissionsByDifficulty.EASY}
+            color={"#29A01A"}
+            bgcolor={"#294d35"}
+          />
+          <div className="flex flex-row justify-between mr-3">
+            <div className="justify-start font-medium text-xs text-neutral-200">
+              Medium
+            </div>
+            <div className="justify-end font-semibold text-xs text-neutral-200">
+              {userSubmissionsByDifficulty.MEDIUM}/
+              {totalQuestionsByDifficultyMap.MEDIUM}
+            </div>
           </div>
-          <ProgressBar Total_Questions={40} questions_completed={20} color={"#B99A16"} bgcolor={"#5e4e25"} />
-          <div className='flex flex-row justify-between mr-3'>
-            <div className='justify-start font-medium text-xs text-neutral-200'  >Hard</div>
-            <div className='justify-end font-semibold text-xs text-neutral-200'>30/50</div>
+          <ProgressBar
+            Total_Questions={totalQuestionsByDifficultyMap.MEDIUM}
+            questions_completed={userSubmissionsByDifficulty.MEDIUM}
+            color={"#B99A16"}
+            bgcolor={"#5e4e25"}
+          />
+          <div className="flex flex-row justify-between mr-3">
+            <div className="justify-start font-medium text-xs text-neutral-200">
+              Hard
+            </div>
+            <div className="justify-end font-semibold text-xs text-neutral-200">
+              {userSubmissionsByDifficulty.HARD}/
+              {totalQuestionsByDifficultyMap.HARD}
+            </div>
           </div>
-          <ProgressBar Total_Questions={50} questions_completed={30} color={"#B91616"} bgcolor={"#5a302f"}/>
+          <ProgressBar
+            Total_Questions={totalQuestionsByDifficultyMap.HARD}
+            questions_completed={userSubmissionsByDifficulty.HARD}
+            color={"#B91616"}
+            bgcolor={"#5a302f"}
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default stats;
