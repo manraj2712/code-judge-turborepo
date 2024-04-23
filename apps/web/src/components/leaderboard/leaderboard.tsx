a5b485c5d01e728a8101d707ef87fb1092da90ed
Aarjav638 <anshjain638@gmail.com>
2024-04-23 20:19:51 +0530

    corrected coloring

--- apps/web/src/components/leaderboard/leaderboard.tsx
+++ apps/web/src/components/leaderboard/leaderboard.tsx
@@ -9,15 +9,29 @@ const leaderBoard = () => {
   const [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
     const fetchUsers = async () => {
-      const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/leaderboard`;
-      const res = await axios({
-        method: "get",
-        url,
-      });
-      const sortedUsers = [...res.data].sort(
-        (a: User, b: User) => b.submissions.length - a.submissions.length
-      );
-      setUsers(sortedUsers);
+      try {
+        const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/leaderboard`;
+        const res = await axios.get(url);
+        const usersWithSubmissions = res.data.filter(
+          (user: User) => user.submissions.length > 0
+        );
+        const sortedUsers = [...usersWithSubmissions].sort(
+          (a: User, b: User) => b.submissions.length - a.submissions.length
+        );
+        let rank = 1;
+        let prevSubmissionsCount = sortedUsers[0].submissions.length;
+        sortedUsers.forEach((user, index) => {
+          if (user.submissions.length < prevSubmissionsCount) {
+            rank = index + 1;
+            prevSubmissionsCount = user.submissions.length;
+          }
+          user.rank = rank;
+        });
+
+        setUsers(sortedUsers);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
     };
 
     fetchUsers();
@@ -32,19 +46,24 @@ const leaderBoard = () => {
     user.rank = rank;
   });
   return (
-    <div className="m-4 rounded-md overflow-hidden shadow-lg">
-      <table className="w-full">
-        <thead className="border">
-          <tr className="text-md font-semibold tracking-wide text-center text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+    <div className="m-4 rounded-md overflow-hidden">
+      <table className="table-fixed overflow-y-scroll lg:table-auto min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+        <thead>
+          <tr className="text-md font-semibold tracking-wide text-center text-gray-900 bg-gray-100 uppercase ">
             <th className="px-4 py-3">Rankings</th>
-            <th className="px-4 py-3">Name</th>
-            <th className="hidden sm:table-cell ">Accepted Submissions</th>
+            <th className="px-4 py-3  ">Name</th>
+            <th className="hidden sm:table-cell px-4 py-3 ">
+              Accepted Submissions
+            </th>
           </tr>
         </thead>
-        <tbody className="bg-white">
+        <tbody>
           {users.map((user, index) => (
-            <tr key={index} className="text-gray-700">
-              <td className="px-4 py-3 text-ms font-semibold border text-center">
+            <tr
+              key={index}
+              className="odd:bg-neutral-900 even:bg-neutral-700  text-gray-200"
+            >
+              <td className="px-4 py-3 text-ms font-semibold border-r text-center  ">
                 <FontAwesomeIcon
                   icon={faTrophy}
                   className="mr-2 text-sm"
@@ -58,10 +77,10 @@ const leaderBoard = () => {
                 />
                 {user.rank}
               </td>
-              <td className="px-4 py-3 text-ms font-semibold border text-center">
+              <td className="px-4 py-3 text-ms  font-semibold border-r-0 sm:border-r text-center ">
                 {user.name}
               </td>
-              <td className=" hidden sm:table-cell px-4 py-3 text-ms font-semibold border text-center">
+              <td className="hidden sm:table-cell  px-4 py-3 text-ms font-semibold text-center ">
                 {user.submissions.length}
               </td>
             </tr>
