e96a486c47dde64b84291c116874c8a1755defec
Aarjav638 <anshjain638@gmail.com>
2024-04-25 18:10:38 +0530

    adjust the width of the column

--- apps/web/src/app/leaderboard/loading1.tsx
+++ apps/web/src/app/leaderboard/loading1.tsx
@@ -9,52 +9,60 @@ const Loading = () => {
   // Generate dummy shimmer blocks
   const shimmerBlocks = Array.from({ length: 10 }).map((_, index) => (
     <tr key={index} className="odd:bg-neutral-900  animate-pulse ">
-      <td className="px-4 py-3 text-center align-middle">
-        <div className="w-[50%]  h-2 bg-gray-400  mx-auto"></div>
+      <td className="text-ms font-semibold py-3  border-r">
+        <div className="w-[50%] rounded-md  h-2 bg-gray-400  mx-auto"></div>
       </td>
-      <td className="px-4 py-3 text-center align-middle">
-        <div className="w-[50%] h-2 bg-gray-400 mx-auto"></div>
+      <td className="text-ms  font-semibold py-3  border-r">
+        <div className="w-[50%] rounded-md h-2 bg-gray-400 mx-auto"></div>
       </td>
-      <td className="px-4 py-3 text-center align-middle">
-        <div className="w-[50%] h-2 bg-gray-400 mx-auto"></div>
+      <td className="text-ms font-semibold py-3 ">
+        <div className="w-[50%] rounded-md h-2 bg-gray-400 mx-auto"></div>
       </td>
     </tr>
   ));
   return (
-    <>
-      <div className="flex justify-center items-center mt-8 gap-4 ">
-        <h1 className="font-bold text-2xl ">
-          Leaderboard <FontAwesomeIcon icon={faTrophy} color="gold" />
-        </h1>
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <div className=" flex flex-row my-8 justify-center items-center ">
+        <h1 className="text-2xl font-semibold text-center">Leaderboard</h1>
+        <FontAwesomeIcon
+          icon={faTrophy}
+          className="ml-3"
+          size="lg"
+          color="gold"
+        />
       </div>
-      <div className="flex flex-col m-4 rounded-md overflow-hidden">
-        <table className="table-fixed overflow-y-scroll lg:table-auto min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="flex  w-[80%] overflow-hidden rounded-md ">
+        <table className="table-fixed overflow-y-scroll lg:table-fixed divide-y w-full divide-gray-200 dark:divide-gray-700">
           <thead>
             <tr className="text-md font-semibold tracking-wide text-center text-gray-900 bg-gray-100 uppercase ">
-              <th className="px-4 py-3">Rankings</th>
-              <th className="px-4 py-3">Name</th>
-              <th className="hidden sm:table-cell px-4 py-3 ">
-                Accepted Submissions
+              <th className=" w-8 sm:w-12 px-3 py-3 truncate ">Rankings</th>
+              <th className="px-3 py-3 w-20 truncate ">Name</th>
+              <th className="w-8 sm:w-12 px-3 py-3 truncate ">
+                Solved Problems
               </th>
             </tr>
           </thead>
           <tbody>{shimmerBlocks}</tbody>
         </table>
-        <div className="flex items-center justify-center my-5">
-          <FontAwesomeIcon
-            icon={faArrowLeft}
-            className="mr-2 text-sm"
-            color="gray"
-          />
-          <span className="mx-4">Page 1 of 1</span>
-          <FontAwesomeIcon
-            icon={faArrowRight}
-            className="ml-2 text-sm"
-            color="gray"
-          />
-        </div>
       </div>
-    </>
+      <div className="flex items-center mt-16 mb-8 justify-center">
+        <FontAwesomeIcon
+          //left arrow
+          icon={faArrowLeft}
+          className="mr-2 "
+          size="sm"
+          color={"dark grey"}
+        />
+        <span className=" mx-4 text-center ">Page 1 of 1</span>
+        <FontAwesomeIcon
+          //right arrow
+          icon={faArrowRight}
+          className="ml-2"
+          color="dark grey"
+          size="sm"
+        />
+      </div>
+    </div>
   );
 };
 export default Loading;
--- apps/web/src/components/leaderboard/leaderboard.tsx
+++ apps/web/src/components/leaderboard/leaderboard.tsx
@@ -54,90 +54,96 @@ const Leaderboard = () => {
     return <Loading />;
   }
   return (
-    <>
-      <div className="flex justify-center items-center mt-8 gap-4 ">
-        <h1 className=" font-bold text-2xl ">
-          Leaderboard <FontAwesomeIcon icon={faTrophy} color="gold" size="lg" />
-        </h1>
+    <div className="flex flex-1 flex-col items-center">
+      <div className=" flex flex-row my-8 justify-center items-center ">
+        <h1 className="text-2xl font-semibold text-center">Leaderboard</h1>
+        <FontAwesomeIcon
+          icon={faTrophy}
+          className="ml-3"
+          size="lg"
+          color="gold"
+        />
       </div>
-      <div className="flex flex-col m-4 rounded-md overflow-hidden">
-        <div className="rounded-md overflow-hidden">
-          <table className="table-fixed overflow-y-scroll lg:table-fixed min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-            <thead>
-              <tr className="text-md font-semibold tracking-wide text-center text-gray-900 bg-gray-100 uppercase ">
-                <th className="px-4 py-3">Rankings</th>
-                <th className="px-4 py-3  ">Name</th>
-                <th className="hidden sm:table-cell px-4 py-3 ">
-                  Accepted Submissions
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user, index) => (
-                <tr
-                  key={index}
-                  className="odd:bg-neutral-900 even:bg-neutral-700  text-gray-200"
-                >
-                  <td className="px-4 py-3 text-ms font-semibold border-r text-center  ">
-                    <FontAwesomeIcon
-                      icon={faTrophy}
-                      className={`mr-2 text-sm ${
-                        (currentPage - 1) * offset + index + 1 < 4
-                          ? "visible"
-                          : "hidden"
-                      }`}
-                      color={
-                        (currentPage - 1) * offset + index + 1 === 1
-                          ? "gold"
-                          : (currentPage - 1) * offset + index + 1 === 2
-                            ? "silver"
-                            : (currentPage - 1) * offset + index + 1 === 3
-                              ? "orange"
-                              : undefined
-                      }
-                    />
+      <div className="flex  w-[80%] overflow-hidden rounded-md ">
+        <table className="table-fixed overflow-y-scroll lg:table-fixed divide-y w-full divide-gray-200 dark:divide-gray-700">
+          <thead>
+            <tr className="text-md font-semibold tracking-wide text-center text-gray-900 bg-gray-100 uppercase ">
+              <th className=" w-8 sm:w-12 px-3 py-3 truncate ">Rankings</th>
+              <th className="px-3 py-3 w-20 truncate ">Name</th>
+              <th className="w-8 sm:w-12 px-3 py-3 truncate ">
+                Solved Problems
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr
+                key={index}
+                className="odd:bg-neutral-900 even:bg-neutral-700 text-center items-center  justify-center  text-gray-200"
+              >
+                <td className="text-ms font-semibold py-3 px-2 border-r">
+                  <FontAwesomeIcon
+                    icon={faTrophy}
+                    className={`text-sm ${
+                      (currentPage - 1) * offset + index + 1 < 4
+                        ? "visible"
+                        : "hidden"
+                    }`}
+                    color={
+                      (currentPage - 1) * offset + index + 1 === 1
+                        ? "gold"
+                        : (currentPage - 1) * offset + index + 1 === 2
+                          ? "silver"
+                          : (currentPage - 1) * offset + index + 1 === 3
+                            ? "orange"
+                            : undefined
+                    }
+                  />
+                  <span className="mx-1">
                     {(currentPage - 1) * offset + index + 1}
-                  </td>
-                  <td className="px-4 py-3 text-ms  font-semibold border-r-0 sm:border-r text-center ">
-                    {user.name}
-                  </td>
-                  <td className="hidden sm:table-cell  px-4 py-3 text-ms font-semibold text-center ">
-                    {user.submissions.length}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="flex item-center m-8 justify-center ">
-          <button
-            onClick={() => goToPage(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            <FontAwesomeIcon
-              //left arrow
-              icon={faArrowLeft}
-              className="mr-2 text-sm"
-              color={currentPage == 1 ? "grey" : "dark grey"}
-            />
-          </button>
-          <span className=" mx-4 ">
-            Page {currentPage} of {totalPages}
-          </span>
-          <button
-            onClick={() => goToPage(currentPage + 1)}
-            disabled={currentPage === totalPages}
-          >
-            <FontAwesomeIcon
-              //right arrow
-              icon={faArrowRight}
-              className="ml-2 text-sm"
-              color={currentPage == totalPages ? "grey" : "dark grey"}
-            />
-          </button>
-        </div>
+                  </span>
+                </td>
+                <td className="text-ms font-semibold py-3 px-2  border-r">
+                  {user.name}
+                </td>
+                <td className="text-ms py-3 px-2  font-semibold">
+                  {user.submissions.length}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    </>
+      <div className="flex items-center mt-16 mb-8 justify-center ">
+        <button
+          onClick={() => goToPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          <FontAwesomeIcon
+            //left arrow
+            icon={faArrowLeft}
+            className="mr-2"
+            size="sm"
+            color={currentPage == 1 ? "grey" : "dark grey"}
+          />
+        </button>
+        <span className=" mx-4 ">
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          onClick={() => goToPage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          <FontAwesomeIcon
+            //right arrow
+            icon={faArrowRight}
+            className="ml-2"
+            size="sm"
+            color={currentPage == totalPages ? "grey" : "dark grey"}
+          />
+        </button>
+      </div>
+    </div>
   );
 };
 
